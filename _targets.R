e421e6f6861922f4f34f306d10efe78a6938f678
Philipp Bayer <philippbay@gmail.com>
2023-09-26 14:43:02 +0800

    Update functions

--- _targets.R
+++ _targets.R
@@ -1,11 +1,20 @@
 library(targets)
 library(tarchetypes)
 source('code/functions.R')
+
 tar_option_set(packages = c('tidyverse', 'ggdendro'))
+
 list(
   tar_target(classifier_distances, 'data/distances.csv', format='file'), # designate the file
   tar_target(classifier_data, get_classifier_data(classifier_distances)), # load the file
   tar_target(dendrogram, plot_dendrogram(classifier_data)), # make the plot
   tar_target(saved, save_plot(dendrogram)), # save the figure
-  tar_render(report, 'code/report.Rmd')
+  tar_render(report, 'code/report.Rmd'),
+  tar_target(blast_hits, '/home/pbayer/Data/runClassifiers/1-BLAST/all_results.tsv', format='file'),
+  tar_target(blast_hits_data, get_hits_data(blast_hits)),
+  tar_target(subsambled_blast_hits_data, subsample_blast_hits(blast_hits_data)),
+  tar_target(blast_hits_plot, plot_blast_hits(blast_hits_data)),
+  tar_target(kraken_hits, '/home/pbayer/Data/runClassifiers/7-kraken/combined.tsv', format = 'file'),
+  tar_target(kraken_hits_data, get_hits_data(kraken_hits)),
+  tar_target(subsambled_kraken_hits_data, subsample_blast_hits(kraken_hits_data))
 )
--- code/functions.R
+++ code/functions.R
@@ -13,3 +13,28 @@ save_plot <- function(plot) {
   ggsave(filename = 'figures/classifier_dendrogram.png',
           plot = plot, dpi = 300)
 }
+
+get_hits_data <- function(file) {
+  read_tsv(file)
+}
+
+subsample_blast_hits <- function(data) {
+  data |> 
+    filter(Subject %in% c('16S_v04_final.fasta', '12s_v010_final.fasta')) |> 
+    filter(Query %in% c('make_12s_16s_simulated_reads_5-BetterDatabaseARTSimulation_runEDNAFLOW_16S_Lulu_RESULTS_dada2_asv.fa', 
+                        'make_12s_16s_simulated_reads_5-BetterDatabaseARTSimulation_runEDNAFLOW_12S_Lulu_RESULTS_dada2_asv.fa')) |> 
+    filter( (Subject == '12s_v010_final.fasta' & 
+               Query == 'make_12s_16s_simulated_reads_5-BetterDatabaseARTSimulation_runEDNAFLOW_12S_Lulu_RESULTS_dada2_asv.fa') | 
+              ( Subject == '16S_v04_final.fasta' &
+                  Query == 'make_12s_16s_simulated_reads_5-BetterDatabaseARTSimulation_runEDNAFLOW_16S_Lulu_RESULTS_dada2_asv.fa'))
+}
+
+plot_blast_hits <- function(data) {
+  data |> 
+    mutate(species = case_when ( species != 'dropped' ~ 'hit',
+                                 species == 'dropped' ~ 'dropped')) |> 
+    group_by(Query, Subject, species) |> 
+    count(species) |> 
+    ggplot(aes(x= Query, y = n, fill = species))
+    
+}
