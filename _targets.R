512d808edc4736cfbc34954793e273947426a2e6
Philipp Bayer <philippbay@gmail.com>
2023-10-05 15:50:38 +0800

    add all results file! make them identical! start work on putting all the tables into one!

--- _targets.R
+++ _targets.R
@@ -15,20 +15,27 @@ list(
   tar_render(classifier_complexity, 'code/classifier_complexity.Rmd'),
   # plotting ASVs per dataset
   tar_render(plot_asvs_per_dataset, 'code/asvs_per_data.Rmd'),
-  # plotting classifier species hits
-  tar_target(blast_hits, '/home/pbayer/Data/runClassifiers/1-BLAST/all_results.tsv', format='file'),
+  # plotting classifier species hits - all of this stuff is for the internal presentation,
+  # and to get a feel for the data
+  tar_target(blast_hits, 'data/1-BLAST_all_results.tsv.gz', format='file'),
   tar_target(blast_hits_data, get_hits_data(blast_hits)),
   tar_target(subsambled_blast_hits_data, subsample_blast_hits(blast_hits_data)),
-  tar_target(kraken_hits, '/home/pbayer/Data/runClassifiers/7-kraken/combined.tsv', format = 'file'),
+  tar_target(thirty_subsambled_blast_hits_data, subsample_blast_thirty_hits(blast_hits_data)),
+  tar_target(kraken_hits, 'data/7-kraken_all_results.tsv.gz', format = 'file'),
   tar_target(kraken_hits_data, get_hits_data(kraken_hits)),
   tar_target(subsambled_kraken_hits_data, subsample_blast_hits(kraken_hits_data)),
+  tar_target(thirty_subsampled_kraken_hits_data, subsample_blast_thirty_hits(kraken_hits_data)),
   tar_target(plot_report, 'code/kraken_blast_plots.Rmd'),
-  tar_target(nbc_hits, '/home/pbayer/Data/runClassifiers/8-NBC/all_results.tsv', format='file'),
+  tar_target(nbc_hits, 'data/8-NBC_all_results.tsv.gz', format='file'),
   tar_target(nbc_hits_data, get_hits_data(nbc_hits)),
   tar_target(subsampled_nbc_hits_data, subsample_nbc_hits(nbc_hits_data)),
+  tar_target(thirty_subsampled_nbc_hits_data, subsample_nbc_hits_thirty(nbc_hits_data)),
   tar_render(investigate_nbc, 'code/investigate_nbc.Rmd'),
   # truth set
   tar_target(truth_set, '/home/pbayer/Data/make_12s_16s_simulated_reads/9-assignSpeciesToASVs/truthsets_with_lineages.tsv', format='file'),
   tar_target(truth_set_data, get_truth_data(truth_set)),
-  tar_render(truth_set_investigation, 'code/truth_set_check.Rmd')
+  tar_render(truth_set_investigation, 'code/truth_set_check.Rmd'),
+  # time to merge all results
+  tar_files(all_results, 'data/' |> list.files(full.names = TRUE, pattern = 'tsv.gz$')),
+  tar_target(merged_all_results, load_and_merge_all_results(all_results))
 )
--- code/functions.R
+++ code/functions.R
@@ -25,20 +25,40 @@ get_truth_data <- function(file) {
   df
 }
   
+subsample_blast_thirty_hits <- function(data) {
+  data |> 
+    filter(Subject %in% c('16S_v04_final.fasta_Taxonomies.CountedFams.txt_thirty_subset_8.fasta', 
+                          '12s_v010_final.fasta_Taxonomies.CountedFams.txt_thirty_subset_8.fasta')) |> 
+    filter(Query %in% c('make_12s_16s_simulated_reads_5-BetterDatabaseARTSimulation_runEDNAFLOW_16S_Lulu_RESULTS_dada2_asv.fa', 
+                        'make_12s_16s_simulated_reads_5-BetterDatabaseARTSimulation_runEDNAFLOW_12S_Lulu_RESULTS_dada2_asv.fa')) |> 
+    filter( (Subject == '12s_v010_final.fasta_Taxonomies.CountedFams.txt_thirty_subset_8.fasta' & 
+               Query == 'make_12s_16s_simulated_reads_5-BetterDatabaseARTSimulation_runEDNAFLOW_12S_Lulu_RESULTS_dada2_asv.fa') | 
+              ( Subject == '16S_v04_final.fasta_Taxonomies.CountedFams.txt_thirty_subset_8.fasta' &
+                  Query == 'make_12s_16s_simulated_reads_5-BetterDatabaseARTSimulation_runEDNAFLOW_16S_Lulu_RESULTS_dada2_asv.fa'))
+  
+}
+
 subsample_nbc_hits <- function(data) {
   data |> 
-    filter(Database %in% c('16S_v04_final.fasta_classifier.joblib', '12s_v010_final.fasta_classifier.joblib')) |> 
-    filter(Query %in% c('../make_12s_16s_simulated_reads_5-BetterDatabaseARTSimulation_runEDNAFLOW_16S_Lulu_RESULTS_dada2_asv.fa',
-                        '../make_12s_16s_simulated_reads_5-BetterDatabaseARTSimulation_runEDNAFLOW_12S_Lulu_RESULTS_dada2_asv.fa')) |> 
-    filter( (Database == '12s_v010_final.fasta_classifier.joblib' &
-               Query == '../make_12s_16s_simulated_reads_5-BetterDatabaseARTSimulation_runEDNAFLOW_12S_Lulu_RESULTS_dada2_asv.fa') |
-              (Database == '16S_v04_final.fasta_classifier.joblib' &
-                 Query == '../make_12s_16s_simulated_reads_5-BetterDatabaseARTSimulation_runEDNAFLOW_16S_Lulu_RESULTS_dada2_asv.fa')) |> 
-    mutate(Query = str_replace_all(Query, '../', '')) |> 
-    mutate(Database = str_replace_all(Database, '_classifier.joblib', '')) |> 
-    rename(Subject = Database) |> 
-    rename(OTU = ASV) |> 
-    rename(species = Species)
+    filter(Subject %in% c('16S_v04_final.fasta', '12s_v010_final.fasta')) |> 
+    filter(Query %in% c('make_12s_16s_simulated_reads_5-BetterDatabaseARTSimulation_runEDNAFLOW_16S_Lulu_RESULTS_dada2_asv.fa',
+                        'make_12s_16s_simulated_reads_5-BetterDatabaseARTSimulation_runEDNAFLOW_12S_Lulu_RESULTS_dada2_asv.fa')) |> 
+    filter( (Subject == '12s_v010_final.fasta' &
+               Query == 'make_12s_16s_simulated_reads_5-BetterDatabaseARTSimulation_runEDNAFLOW_12S_Lulu_RESULTS_dada2_asv.fa') |
+              (Subject == '16S_v04_final.fasta' &
+                 Query == 'make_12s_16s_simulated_reads_5-BetterDatabaseARTSimulation_runEDNAFLOW_16S_Lulu_RESULTS_dada2_asv.fa'))
+}
+
+subsample_nbc_hits_thirty <- function(data) {
+  data |> 
+    filter(Subject %in% c('16S_v04_final.fasta_Taxonomies.CountedFams.txt_thirty_subset_4.fasta',
+                           '12s_v010_final.fasta_Taxonomies.CountedFams.txt_thirty_subset_4.fasta')) |> 
+    filter(Query %in% c('make_12s_16s_simulated_reads_5-BetterDatabaseARTSimulation_runEDNAFLOW_16S_Lulu_RESULTS_dada2_asv.fa',
+                        'make_12s_16s_simulated_reads_5-BetterDatabaseARTSimulation_runEDNAFLOW_12S_Lulu_RESULTS_dada2_asv.fa')) |> 
+    filter( (Subject == '12s_v010_final.fasta_Taxonomies.CountedFams.txt_thirty_subset_4.fasta' &
+               Query == 'make_12s_16s_simulated_reads_5-BetterDatabaseARTSimulation_runEDNAFLOW_12S_Lulu_RESULTS_dada2_asv.fa') |
+              (Subject == '16S_v04_final.fasta_Taxonomies.CountedFams.txt_thirty_subset_4.fasta' &
+                 Query == 'make_12s_16s_simulated_reads_5-BetterDatabaseARTSimulation_runEDNAFLOW_16S_Lulu_RESULTS_dada2_asv.fa')) 
 }
 
 subsample_blast_hits <- function(data) {
@@ -50,4 +70,16 @@ subsample_blast_hits <- function(data) {
                Query == 'make_12s_16s_simulated_reads_5-BetterDatabaseARTSimulation_runEDNAFLOW_12S_Lulu_RESULTS_dada2_asv.fa') | 
               ( Subject == '16S_v04_final.fasta' &
                   Query == 'make_12s_16s_simulated_reads_5-BetterDatabaseARTSimulation_runEDNAFLOW_16S_Lulu_RESULTS_dada2_asv.fa'))
+}
+
+
+load_and_merge_all_results <- function(files) {
+  # files is a named list
+  
+  #all_results_ecfccd47                  all_results_3794a5bc                  all_results_0df11726                  all_results_c18f63bd 
+  #"data//1-BLAST_all_results.tsv.gz"  "data//2-mmseqs2_all_results.tsv.gz"   "data//3-Qiime2_all_results.tsv.gz" "data//4-metabuli_all_results.tsv.gz" 
+  #all_results_213d2d72                  all_results_f4773b3e                  all_results_429da257                  all_results_0e9a9698 
+  #"data//5-vsearch_all_results.tsv.gz"   "data//6-mothur_all_results.tsv.gz"   "data//7-kraken_all_results.tsv.gz"      "data//8-NBC_all_results.tsv.gz" 
+  
+  # load it all into one 
 }
